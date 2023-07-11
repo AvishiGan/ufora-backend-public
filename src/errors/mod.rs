98c45a72ab702e0c7b6b436a62f37adc58481905
sineththamuditha <sineththamuditha@gmail.com>
2023-07-11 09:52:01 +0530

    Added authorization errors after fixing authentication error mixup

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -2,8 +2,10 @@
 mod validation_errors;
 mod database_errors;
 mod authentication_errors;
+mod authorization_errors;
 
 
 use validation_errors::ValidationError as VALIDATIONERROR;
 use database_errors::DatabaseError as DATABASEERROR;
 use authentication_errors::AuthenticationError as AUTHENTICATIONERROR;
+use authorization_errors::AuthorizationError as AUTHORIZATIONERROR;
