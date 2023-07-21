fc74ee2536b72edd4db6764f2a1c7bef29e1b11d
sineththamuditha <sineththamuditha@gmail.com>
2023-07-21 13:27:13 +0530

    Feature: registration of an undergraduate added

--- src/handlers/registration_handlers.rs
+++ src/handlers/registration_handlers.rs
@@ -4,70 +4,81 @@ use axum::{http::StatusCode, response::IntoResponse, Json, extract::State};
 
 use surrealdb::{Surreal, engine::remote::ws::Client, sql::Thing};
 
-use crate::models::{company::Company,user::User};
+use crate::models::{company::Company,undergraduate::Undergraduate,user::User};
 
-#[derive(serde::Deserialize,serde::Serialize,Debug)]
-pub struct Undergraduate {
-    username: String,
-    password: String,
-    email: Option<String>,
-    phone: Option<String>,
-}
-
-impl Undergraduate {
-
-    fn hash_password(mut self) -> Self {
-        self.password = hash_password(self.password).unwrap();
-        self
-    }
-
-}
-
-pub async fn register_an_undergraduate(
-    State(db): State<Arc<Surreal<Client>>>,
-    Json(new_user): Json<Undergraduate>
-) -> Result<impl IntoResponse,StatusCode> {
-
-    let new_user = new_user.hash_password();
-
-    println!("{:?}", new_user);
-
-    let undergraduate:Undergraduate = db
-        .create("user")
-        .content(new_user)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    
-
-    Ok("User created successfully")
-}
-
-fn hash_password(password: String) -> Result<String,StatusCode> {
-    bcrypt::hash(password, 14).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
-}
-
+// request struct for registration of an undergraduate
 #[derive(serde::Deserialize)]
-pub struct CompanyRequestDetails {
+pub struct UndergraduateRegistrationRequest {
     name:Option<String>,
     username:Option<String>,
     email:Option<String>,
     password:Option<String>,
 }
 
-impl CompanyRequestDetails {
+impl UndergraduateRegistrationRequest {
+    pub fn get_undergraduate_and_user_models(self) -> (Undergraduate,User) {
+        (Undergraduate::from(self.name,self.email),User::from(self.username,self.password))
+    }
+}
+
+// response struct for registration of an undergraduate
+#[derive(serde::Serialize)]
+pub struct UndergraduateRegistrationResponse {
+    message:String
+}
+
+// handler for registration of an undergraduate
+pub async fn register_an_undergraduate(
+    State(db): State<Arc<Surreal<Client>>>,
+    Json(company_details): Json<UndergraduateRegistrationRequest>,
+) -> Result<Json<UndergraduateRegistrationResponse>,StatusCode> {
+
+    let (undergraduate,user) = company_details.get_undergraduate_and_user_models();
+
+    let response = db.query(undergraduate.get_register_query().await.unwrap()).await;
+
+    match response {
+        Ok(mut response) => {
+
+            let undergraduate_id:Result<Option<Thing>,surrealdb::Error> = response.take(0);
+            let response = db.query(user.get_create_user_query("undergraduate".to_string(),undergraduate_id.unwrap()).await.unwrap()).await;
+            
+            match response {
+                Ok(_) => Ok(Json(UndergraduateRegistrationResponse {message:"Undergraduate account has been created successfully".to_string()})),
+                Err(e) => {println!("{:?}",e); Err(StatusCode::INTERNAL_SERVER_ERROR)}
+            }
+
+        },
+        Err(e) => {println!("{:?}",e); Err(StatusCode::INTERNAL_SERVER_ERROR)}
+    }
+
+}
+
+// request struct for registration of a company
+#[derive(serde::Deserialize)]
+pub struct CompanyRegistrationRequest {
+    name:Option<String>,
+    username:Option<String>,
+    email:Option<String>,
+    password:Option<String>,
+}
+
+impl CompanyRegistrationRequest {
     pub fn get_company_and_user_models(self) -> (Company,User) {
         (Company::from(self.name,self.email),User::from(self.username,self.password))
     }
 }
 
+// response struct for registration of a company
 #[derive(serde::Serialize)]
 pub struct CompanyRegistrationResponse {
     message:String
 }
 
+// handler for registration of a company
 pub async fn register_a_company(
     State(db): State<Arc<Surreal<Client>>>,
-    Json(company_details): Json<CompanyRequestDetails>,
+    Json(company_details): Json<CompanyRegistrationRequest>,
 ) -> Result<Json<CompanyRegistrationResponse>,StatusCode> {
 
     let (company,user) = company_details.get_company_and_user_models();
@@ -82,11 +93,11 @@ pub async fn register_a_company(
             
             match response {
                 Ok(_) => Ok(Json(CompanyRegistrationResponse { message: "Company account has been created successfully".to_string() })),
-                Err(e) => {println!("{:?}",e);return Err(StatusCode::INTERNAL_SERVER_ERROR)}
+                Err(e) => {println!("{:?}",e); Err(StatusCode::INTERNAL_SERVER_ERROR)}
             }
 
         },
-        Err(e) => {println!("{:?}",e);return Err(StatusCode::INTERNAL_SERVER_ERROR)}
+        Err(e) => {println!("{:?}",e); Err(StatusCode::INTERNAL_SERVER_ERROR)}
     }
 
 }
