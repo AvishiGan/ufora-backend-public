f880f3c1b29c06de43701f1a352860d4a687dc5e
sineththamuditha <sineththamuditha@gmail.com>
2023-07-21 11:12:33 +0530

    Model: undergraduate model added

--- src/models/undergraduate.rs
+++ src/models/undergraduate.rs
@@ -1,54 +1,37 @@
-use std::sync::Arc;
 use simple_collection_macros::bmap;
 
 use axum::http::StatusCode;
-use surrealdb::{sql::{
-    Thing, 
-    statements::{CreateStatement, InsertStatement},
-    Output,
-    Fields,
-    Field,
-    Values,
-    Value,
-    Object,
-    Strand,
-    Table,
-    Data,
-    Idiom, Part, Ident
-}, Surreal, engine::remote::ws::Client};
+use::surrealdb::sql::{Thing,Table,Object,Value,Part,Fields,Field,Ident,Idiom,Output,Data,Values,Strand,
+    statements::CreateStatement
+};
 
-
-
-#[derive(serde::Serialize,serde::Deserialize)]
-pub struct Company {
+pub struct Undergraduate {
     id: Option<Thing>,
     name: Option<String>,
-    email: Option<String>
+    email: Option<String>,
 }
 
-impl Company {
-
-    pub fn from(name: Option<String>, email: Option<String> ) -> Self {
+impl Undergraduate {
+    pub fn from(name: Option<String>, email: Option<String>) -> Self {
         Self {
-            id:None,
+            id: None,
             name,
-            email
+            email,
         }
     }
 
     pub async fn get_register_query(
-        self,
-    ) -> Result<CreateStatement,StatusCode> {
+        self
+     ) -> Result<CreateStatement,StatusCode> {
 
-
-        match (self.email.clone(),self.name.clone()) {
-            (None,_) | (_,None) => Err(StatusCode::INTERNAL_SERVER_ERROR) ?,
+        match (self.name.clone(),self.email.clone()) {
+            (None,_) | (_, None) => Err(StatusCode::INTERNAL_SERVER_ERROR) ?,
             (_,_) => {}
         }
 
         Ok(CreateStatement {
             what: Values(
-                vec![Value::Table(Table("company".to_string()))]
+                vec![Value::Table(Table("undergraduate".to_string()))]
             ),
             data: Some(Data::ContentExpression(Value::Object( Object (bmap! {
                 "name".to_string() => Value::Strand(Strand(self.name.unwrap())),
