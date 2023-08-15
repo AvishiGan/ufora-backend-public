a12a7dbaecdd4a9d498094f563d871d23ba8ac0c
AbdullahJasmin <abdullahmohjasmin@gmail.com>
2023-08-15 14:10:38 +0530

    add profile retrieval with username or email

--- src/handlers/profile_handlers.rs
+++ src/handlers/profile_handlers.rs
@@ -1,114 +1,252 @@
+use core::borrow;
 use std::sync::Arc;
 
-use axum::{http::StatusCode,Json, extract::State};
+use axum::{extract::State, http::StatusCode, Json};
 
-use serde::{Serialize, Deserialize};
-use surrealdb::{Surreal, engine::remote::ws::Client, sql::{Thing, Id}};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use surrealdb::{
+    engine::remote::ws::Client,
+    sql::{Id, Thing},
+    Surreal,
+};
 
-use crate::models::profile::Profile;
+use crate::models::{profile::Profile, user::{User, self}};
 
-// use crate::models::{company::Company,undergraduate::Undergraduate,user::User};
+fn merge(a: Value, b: Value) -> Value {
+    match (a, b) {
+        (Value::Object(mut a), Value::Object(b)) => {
+            for (k, v) in b {
+                let entry = a.entry(k).or_insert(Value::Null);
+                *entry = merge(entry.clone(), v);
+            }
+            Value::Object(a)
+        }
+        (_, b) => b,
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 
-pub struct UserProfile{
+pub struct OnlyProfile {
+    name: Option<String>,
     intro: Option<String>,
     profile_pic: Option<String>,
     contact: Option<String>,
     // optional params depending on user
     date_of_birth: Option<String>,
     address: Option<String>,
-    map: Option<String>,
 }
 
-impl UserProfile {
-
-
-    pub fn get_profile_model(self, id:Option<String>) -> Profile {
-        let id = Thing { tb:"profile".to_string(), id:Id::String(id.unwrap()) };
-        Profile::from(Some(id),self.intro,self.profile_pic,self.date_of_birth,self.address,self.contact,self.map)
+impl OnlyProfile {
+    pub fn get_profile_model(self, id: Option<String>) -> Profile {
+        let id = Thing {
+            tb: "profile".to_string(),
+            id: Id::String(id.unwrap()),
+        };
+        Profile::from(
+            Some(id),
+            self.name,
+            self.intro,
+            self.profile_pic,
+            self.date_of_birth,
+            self.address,
+            self.contact,
+        )
     }
-    
 }
 
-// response struct for registration of an undergraduate
+// response struct for all responspones
 #[derive(Serialize)]
 pub struct ProfileResponse {
-    message:String
+    message: String,
 }
 
-#[derive(Serialize,Deserialize,Debug)]
+// Create user profile for logged in user
+#[derive(Serialize, Deserialize, Debug)]
 pub struct ReturnID {
-    id: Thing
+    id: Thing,
 }
-
 
 pub async fn create_profile(
     claim: crate::models::user_claim::Claim,
     State(db): State<Arc<Surreal<Client>>>,
-    Json(profile_details): Json<UserProfile>,
-)-> (StatusCode,Json<ProfileResponse>) {
-
-    println!("{:?}",claim.get_user_id());
+    Json(profile_details): Json<OnlyProfile>,
+) -> (StatusCode, Json<ProfileResponse>) {
+    // println!("{:?}",claim.get_user_id());
     let profile_detail = profile_details.get_profile_model(Some(claim.get_user_id()));
 
-    let response = db.query(profile_detail.get_profile_create_query().await.unwrap()).await;
+    // println!("{:?}",profile_detail);
+    let response = db
+        .query(profile_detail.get_profile_create_query().await.unwrap())
+        .await;
 
-
-        match response{
-            Ok(mut response) => {
-                let ret_id:Result<Option<Thing>,surrealdb::Error> = response.take(0);
-                println!("{:?}",ret_id);
-                if ret_id.is_ok() {
-                    return (StatusCode::OK,Json(ProfileResponse {message:"Profile has been created successfully".to_string()}));
-                }
-                (StatusCode::INTERNAL_SERVER_ERROR,Json(ProfileResponse { 
-                    message: "Profile could not be created".to_string()
-                }))
-            },
-            Err(e) => {println!("{:?}",e); (StatusCode::INTERNAL_SERVER_ERROR,Json(ProfileResponse { 
-                message: "Profile could not be created".to_string()
-            } ))}
-        }   
-
+    match response {
+        Ok(mut response) => {
+            let ret_id: Result<Option<Thing>, surrealdb::Error> = response.take(0);
+            println!("{:?}", ret_id);
+            if ret_id.is_ok() {
+                return (
+                    StatusCode::OK,
+                    Json(ProfileResponse {
+                        message: "Profile has been created successfully".to_string(),
+                    }),
+                );
+            }
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ProfileResponse {
+                    message: "Profile could not be created".to_string(),
+                }),
+            )
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(ProfileResponse {
+                    message: "Profile could not be created".to_string(),
+                }),
+            )
+        }
+    }
 }
 
-#[derive(Serialize,Deserialize,Debug)]
-pub struct ProfileRequest{
-    id: Option<String>
+// Getting the profile using the user ID
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProfileRequest {
+    id: Option<String>,
 }
 
 // return user profile
 pub async fn get_profile(
     State(db): State<Arc<Surreal<Client>>>,
     Json(profile_request): Json<ProfileRequest>,
-) -> (StatusCode,Json<Profile>)
-{
+) -> (StatusCode, Json<Profile>) {
+    println!("{:?}", profile_request);
+    let profile = db
+        .query(
+            Profile::get_profile_by_user_id(profile_request.id)
+                .await
+                .unwrap(),
+        )
+        .await;
 
-    println!("{:?}",profile_request);
-    let profile = db.query(Profile::get_profile_by_user_id(profile_request.id).await.unwrap()).await;
-
-     match profile {
+    match profile {
         Ok(mut response) => {
-            let profile:Result<Option<Profile>,surrealdb::Error> = response.take(0);
+            let profile: Result<Option<Profile>, surrealdb::Error> = response.take(0);
             match profile {
                 Ok(Some(profile)) => {
-                    return (StatusCode::OK,Json(profile));
-                },
+                    return (StatusCode::OK, Json(profile));
+                }
                 Ok(None) => {
-                    return (StatusCode::NOT_FOUND,Json(Profile::default()));
-                },
+                    return (StatusCode::NOT_FOUND, Json(Profile::default()));
+                }
                 Err(e) => {
-                    println!("{:?}",e);
-                    return (StatusCode::INTERNAL_SERVER_ERROR,Json(Profile::default()));
+                    println!("{:?}", e);
+                    return (StatusCode::INTERNAL_SERVER_ERROR, Json(Profile::default()));
                 }
             }
-        },
-        Err(e) => {
-            println!("{:?}",e);
-            return (StatusCode::INTERNAL_SERVER_ERROR,Json(Profile::default()));
         }
-         
-     }
+        Err(e) => {
+            println!("{:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, Json(Profile::default()));
+        }
+    }
+}
 
+// Getting the profile using the username/email and getting the associated profile
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProfileRequestUsername {
+    username: Option<String>,
+    email: Option<String>,
+}
+
+// Object {"address": Null, "contact": String("+94771231234"), "date_of_birth": String("2000-10-29T00:00:00Z"), "email": String("binuridesilva27@gmail.com"), "email_verification_flag": Null, "gmap": Null, "id": String("profile:hlk48mmzp8nva6jg35ti"), "intro": String("hi i am Abdullah"), "invalid_login_attempts": Number(0), "locked_flag": Bool(false), "name": String("Abdullah Jasmin"), "password": String("$2b$14$4C2ZWzlpbjMSlHPpHpl.L.bUHsH0wAFcZmjXZIjlc30i3tvYZ3T/W"), "profile_pic": String("dp"), "user_id": String("undergraduate:hlk48mmzp8nva6jg35ti"), "user_type": String("undergraduate"), "username": String("user1")}
+
+pub async fn get_user_profile(
+    State(db): State<Arc<Surreal<Client>>>,
+    Json(user_profile): Json<ProfileRequestUsername>,
+) -> (StatusCode, Json<Value>) {
+    // println!("{:?}",user_profile);
+
+    // get the user from user model
+    let user =
+        User::get_user_by_email_or_username(db.clone(), user_profile.email, user_profile.username)
+            .await;
+
+    // println!("{:?}",user);
+
+    // error if user not found
+    match &user {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{:?}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(Value::String("User not found".to_string())),
+            );
+        }
+    }
+    
+    // get the user fields to an object
+    let user_fields = user.unwrap();
+
+    // let profile_request = Some(user.unwrap().get_user_id());
+    let profile_id = Some(format!(
+        "profile:{}",
+        user_fields.get_user_id().id.to_string()
+    ));
+    // println!("{:?}",profile_id);
+
+    // get the profile from profile model
+    let profile = db
+        .query(Profile::get_profile_by_user_id(profile_id).await.unwrap())
+        .await;
+
+    // get the profile json values
+    // println!("{:?}",profile);
+
+    // error if profile not found else merge array and print result
+    match profile {
+        Ok(mut response) => {
+            let profile: Result<Option<Profile>, surrealdb::Error> = response.take(0);
+            match profile {
+                Ok(Some(profile)) => {
+                    // merging user array and profile array
+                    let mut user_profile_result = merge(user_fields.into(), profile.into());
+                    
+                    // remove unneccessary field
+                    user_profile_result.as_object_mut().unwrap().remove("password");
+                    user_profile_result.as_object_mut().unwrap().remove("invalid_login_attempts");
+                    user_profile_result.as_object_mut().unwrap().remove("locked_flag");
+                    
+                    
+                    println!("{:?}", user_profile_result);
+                    return (StatusCode::OK, Json(user_profile_result));
+                }
+                Ok(None) => {
+                    return (
+                        StatusCode::NOT_FOUND,
+                        Json(Value::String("Profile not found".to_string())),
+                    );
+                }
+                Err(e) => {
+                    println!("{:?}", e);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json(Value::String(e.to_string())),
+                    );
+                }
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(Value::String(e.to_string())),
+            );
+        }
+    }
 }
--- src/models/profile.rs
+++ src/models/profile.rs
@@ -1,50 +1,63 @@
-use std::sync::Arc;
-
 use crate::services::queryBuilder::{
     get_select_query, Column, Expression, ExpressionConnector, Item,
 };
-use axum::{http::StatusCode, Json};
-use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
+use axum::http::StatusCode;
+use chrono::{DateTime, NaiveDate, Utc};
 
+use dotenvy_macro::dotenv;
 use simple_collection_macros::bmap;
 use surrealdb::sql::{
     statements::CreateStatement, Data, Datetime, Field, Fields, Ident, Idiom, Object, Output, Part,
     Strand, Table, Thing, Value, Values,
 };
 
+fn generate_embed_link(address: Option<String>, api_key: &str) -> String {
+    format!(
+        "https://www.google.com/maps/embed/v1/place?key={}&q={}",
+        api_key,
+        address.unwrap()
+    )
+}
+
 #[derive(serde::Serialize, serde::Deserialize, Default, Debug)]
 pub struct Profile {
     id: Option<Thing>,
+    name: Option<String>,
     intro: Option<String>,
     profile_pic: Option<String>,
     contact: Option<String>,
     // optional params depending on user
     date_of_birth: Option<String>,
     address: Option<String>,
-    map: Option<String>,
+
+    // mutable gmap
+    // gmap: Option<String>,
+    gmap: Option<String>,
 }
 
 impl Profile {
     // returns a new profile model
     pub fn from(
         id: Option<Thing>,
+        name: Option<String>,
         intro: Option<String>,
         profile_pic: Option<String>,
         date_of_birth: Option<String>,
         address: Option<String>,
         contact: Option<String>,
-        map: Option<String>,
     ) -> Self {
         Self {
             id,
+            name,
             intro,
             profile_pic,
             date_of_birth,
             address,
             contact,
-            map,
+            gmap: None,
         }
     }
+
 
     pub async fn get_profile_create_query(self) -> Result<CreateStatement, StatusCode> {
         // println!("{:?}",self);
@@ -53,16 +66,41 @@ impl Profile {
             _ => {}
         }
 
+        let mut map = bmap! {
+            "id".to_string() => Value::Thing(self.id.unwrap()),
+            "name".to_string() => Value::Strand(Strand(self.name.unwrap())),
+            "intro".to_string() => Value::Strand(Strand(self.intro.unwrap())),
+            "profile_pic".to_string() => Value::Strand(Strand(self.profile_pic.unwrap())),
+            "contact".to_string() => Value::Strand(Strand(self.contact.unwrap())),
+        };
+
+        if let Some(date_of_birth) = self.date_of_birth.clone() {
+            map.insert(
+                "date_of_birth".to_string(),
+                Value::Datetime(Datetime(DateTime::<Utc>::from_utc(
+                    NaiveDate::parse_from_str(&date_of_birth, "%d-%m-%Y")
+                        .expect("Invalid date format")
+                        .and_hms_opt(0, 0, 0)
+                        .unwrap(),
+                    Utc,
+                ))),
+            );
+        } 
+
+        if let Some(address) = self.address.clone() {
+            let api_key = dotenv!("MAP_API_KEY");
+            let embed_link = generate_embed_link(Some(address.clone()), api_key).replace(" ", "%20");
+         
+            // add address to profile
+            map.insert("address".to_string(), Value::Strand(Strand(address)));
+
+            // println!("Embed link: {}", embed_link);
+            map.insert("gmap".to_string(), Value::Strand(Strand(embed_link)));
+        }
+
         Ok(CreateStatement {
             what: Values(vec![Value::Table(Table("profile".to_string()))]),
-            data: Some(Data::ContentExpression(Value::Object(Object(bmap! {
-                "id".to_string() => Value::Thing(self.id.unwrap()),
-                "intro".to_string() => Value::Strand(Strand(self.intro.unwrap())),
-                "profile_pic".to_string() => Value::Strand(Strand(self.profile_pic.unwrap())),
-                "date_of_birth".to_string() => Value::Datetime(Datetime(DateTime::<Utc>::from_utc(NaiveDate::parse_from_str(&self.date_of_birth.unwrap(), "%d-%m-%Y").expect("Invalid date format").and_hms(0, 0, 0), Utc))),
-                "contact".to_string() => Value::Strand(Strand(self.contact.unwrap())),
-                "map".to_string() => Value::Strand(Strand(self.map.unwrap())),
-            })))),
+            data: Some(Data::ContentExpression(Value::Object(Object(map)))),
             output: Some(Output::Fields(Fields(
                 vec![Field::Alone(Value::Idiom(Idiom(vec![Part::Field(Ident(
                     "id".to_string(),
@@ -72,19 +110,15 @@ impl Profile {
             timeout: None,
             parallel: false,
         })
+ 
     }
 
-    pub async fn get_profile_by_user_id(user_id: Option<String>) 
-    -> Result<String,StatusCode>
-    {
+    pub async fn get_profile_by_user_id(user_id: Option<String>) -> Result<String, StatusCode> {
         let profile = get_select_query(
             Item::Table("profile".to_string()),
             Column::All,
             Some(vec![(
-                Expression::EqualTo(
-                    "id".to_string(),
-                    user_id.unwrap(),
-                ),
+                Expression::EqualTo("id".to_string(), user_id.unwrap()),
                 ExpressionConnector::End,
             )]),
             None,
@@ -93,8 +127,23 @@ impl Profile {
             None,
         );
 
-        println!("{:?}", profile); 
+        // println!("{:?}", profile);
         Ok(profile)
+    }
+}
 
+
+impl Into<serde_json::Value> for Profile {
+    fn into(self) -> serde_json::Value {
+        serde_json::json!({
+            "id": self.id,
+            "name": self.name,
+            "intro": self.intro,
+            "profile_pic": self.profile_pic,
+            "contact": self.contact,
+            "date_of_birth": self.date_of_birth,
+            "address": self.address,
+            "gmap": self.gmap
+        })
     }
 }
